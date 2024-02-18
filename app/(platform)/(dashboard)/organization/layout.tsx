0f80a6039c6de50dc66ebfa30cc72c6b8538467f
Nick Engelhardt <enickgoku2@gmail.com>
2024-02-18 12:17:47 -0500

    sidebar

--- app/(platform)/(dashboard)/organization/layout.tsx
+++ app/(platform)/(dashboard)/organization/layout.tsx
@@ -1,8 +1,12 @@
+import { Sidebar } from '@/app/(platform)/(dashboard)/_components/sidebar';
+
 const OrganizationLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="pt-14 md:pt-20 px-4 max-w-6xl 2xl:max-w-screen-xl mx-auto">
       <div className="flex gap-x-5">
-        <div className="w-64 shrink-0 hidden md:block">{/* sidebar */}</div>
+        <div className="w-64 shrink-0 hidden md:block">
+          <Sidebar />
+        </div>
         {children}
       </div>
     </main>
