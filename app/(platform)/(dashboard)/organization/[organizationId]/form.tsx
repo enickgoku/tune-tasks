6742df82041bdccd2258a4a42ef5b45bd973b95e
Nick Engelhardt <enickgoku2@gmail.com>
2024-02-19 18:29:28 -0500

    added custom hook and handler for server action

--- app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
@@ -1,20 +1,30 @@
 'use client';
 
-import { State, createBoard } from '@/actions/create-board';
-import { Button } from '@/components/ui/button';
-import { useFormState } from 'react-dom';
+import { createBoard } from '@/actions/create-board';
 import { FormInput } from '@/app/(platform)/(dashboard)/organization/[organizationId]/input';
 import { FormButton } from './form-button';
+import { useAction } from '@/hooks/use-action';
 
 export const Form = () => {
-  const initialState = { message: null, errors: {} } as State;
+  const { execute, fieldErrors } = useAction(createBoard, {
+    onSuccess: (data) => {
+      console.log('Board created', data);
+    },
+    onError: (error) => {
+      console.error('Failed to create board', error);
+    },
+  });
 
-  const [state, dispatch] = useFormState(createBoard, initialState);
+  const onSubmit = (formData: FormData) => {
+    const title = formData.get('title') as string;
+
+    execute({ title });
+  };
 
   return (
-    <form action={dispatch}>
+    <form action={onSubmit}>
       <div className="flex flex-col space-y-2">
-        <FormInput errors={state?.errors} />
+        <FormInput errors={fieldErrors} />
       </div>
       <FormButton />
     </form>
